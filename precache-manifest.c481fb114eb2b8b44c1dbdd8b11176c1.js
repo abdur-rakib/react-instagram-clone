2bffc02257beff5a23417dfd6629738abff56737
abdur-rakib <abdurrakib961@gmail.com>
2020-07-28 18:45:43 +0600

    Updates

---
+++ precache-manifest.c481fb114eb2b8b44c1dbdd8b11176c1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61f04219004b559b50e31df5934146a9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "751b8e10bc7ffa466e81",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "7147c1103e40af31e819",
+    "url": "/static/css/main.30d5d9dc.chunk.css"
+  },
+  {
+    "revision": "751b8e10bc7ffa466e81",
+    "url": "/static/js/2.b4902b9a.chunk.js"
+  },
+  {
+    "revision": "544111c69119227bbf075a65771ccdb1",
+    "url": "/static/js/2.b4902b9a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7147c1103e40af31e819",
+    "url": "/static/js/main.ba4dd133.chunk.js"
+  },
+  {
+    "revision": "83577550cf23ef276fd4",
+    "url": "/static/js/runtime-main.7a42d414.js"
+  }
+]);
